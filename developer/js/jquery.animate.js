3faf62b9127f82908c817a93e67618bf19cc0369
Dawid Rybacki <dawidryba11@gmail.com>
2016-11-18 15:01:18 +0100

    Add file all sizes

--- developer/js/jquery.animate.js
+++ developer/js/jquery.animate.js
@@ -1,3 +1,15 @@
 jQuery(document).ready(function($) {
-    
+    function startAnimate(){
+        $('.logo, .slogan1, .slogan2, .link, .picture, .click, .sep, .fir, .sec, .thr').addClass('notrans');
+        $('.logo, .slogan1, .slogan2, .link, .picture, .click, .sep, .fir, .sec, .thr').removeClass('animate');
+        setTimeout(function(){
+            $('.logo, .slogan1, .slogan2, .link, .picture, .click, .sep, .fir, .sec, .thr').removeClass('notrans');
+            $('.logo, .slogan1, .slogan2, .link, .picture, .click, .sep, .fir, .sec, .thr').addClass('animate');
+        },1000);
+    }
+
+    startAnimate();
+    var interval = setInterval(startAnimate, 12000);
+    //$('.logo, .slogan1, .slogan2, .link, .picture, .click, .sep, .fir, .sec, .thr').addClass('notrans');
+    //$('.logo, .slogan1, .slogan2, .link, .picture, .click, .sep, .fir, .sec, .thr').addClass('animate');
 });
